89700ac2ab755fa5d7af39150249bfb6daaa6e77
zhonglunsheng <873946453@qq.com>
2018-10-26 14:58:01 +0800

    单体项目maven分层

---
+++ house-biz/src/test/java/com/lsheng/house/biz/HouseBizApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lsheng.house.biz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HouseBizApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ house-web/src/main/java/com/lsheng/house/HouseApplication.java
@@ -0,0 +1,18 @@
+package com.lsheng.house;
+
+/**
+ * @author zhonglunsheng
+ * @Description
+ * @create 2018-10-26 14:30
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseApplication.class, args);
+    }
+}
+
